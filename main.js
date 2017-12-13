5e9d296ee0c33fb64ed785153692cd117460b6f6
Jay Kim <jkim208@masonlive.gmu.edu>
2017-12-13 14:20:19 -0500

    Added menu template

--- main.js
+++ main.js
@@ -1,8 +1,40 @@
-// Main.js
 const electron = require('electron');
-const url = require('url');
 const path = require('path');
+const url = require('url');
 
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, Menu} = electron;
 
-let mainWindow
+let mainWindow;
+
+// Listen for app to be ready
+app.on('ready', function(){
+  // Create new window
+  mainWindow = new BrowserWindow({});
+  // Load html in window
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'mainWindow.html'),
+    protocol: 'file:',
+    slashes:true
+  }));
+
+  // Build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+  // Insert Menu
+  Menu.setApplicationMenu(mainMenu);
+});
+
+// Create menu template
+const mainMenuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Add Item',
+                click() {
+                    createAddWindow();
+                }
+            }
+        ]
+    }
+]
