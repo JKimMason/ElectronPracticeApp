09f6046a48f2ba1cedafcbc7d457b4d28478e95d
Jay Kim <jkim208@masonlive.gmu.edu>
2017-12-13 14:27:31 -0500

    Added createAddWindow function

--- main.js
+++ main.js
@@ -5,24 +5,42 @@ const url = require('url');
 const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
+let addWindow;
 
 // Listen for app to be ready
 app.on('ready', function(){
-  // Create new window
-  mainWindow = new BrowserWindow({});
-  // Load html in window
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'mainWindow.html'),
-    protocol: 'file:',
-    slashes:true
-  }));
+    // Create new window
+    mainWindow = new BrowserWindow({});
+    // Load html in window
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file:',
+        slashes:true
+    }));
+    // Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
 
-  // Build menu from template
-  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
-
-  // Insert Menu
-  Menu.setApplicationMenu(mainMenu);
+    // Insert Menu
+    Menu.setApplicationMenu(mainMenu);
 });
+
+// Handle add item window
+function createAddWindow(){
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: Add Shopping List Item,
+    });
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'addWindow.html'),
+        protocol: 'file: ',
+        slashes: true
+    }));
+    // Handle garbage Collection
+    addWindow.on('close', function(){
+        addWindow = null;
+    });
+}
 
 // Create menu template
 const mainMenuTemplate = [
@@ -33,6 +51,19 @@ const mainMenuTemplate = [
                 label: 'Add Item',
                 click() {
                     createAddWindow();
+                }
+            },
+            {
+                label: 'Clear Items',
+                click() {
+                    mainWindow.webContents.send('item:clear');
+                }
+            },
+            {
+                label: 'Quit',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                click(){
+                    app.quit();
                 }
             }
         ]
